9e228b7a7761c022fadb9917435795211245afc9
Jessica Halley <jmmhalley@gmail.com>
2017-06-14 15:55:41 +1200

    Create itemClass.js

--- server/db/itemClass.js
+++ server/db/itemClass.js
@@ -1,6 +1,6 @@
 const getItemClass = (db) => {
   return db('itemClass')
-  .join('categories','itemClass.categories_id',"=","categories.categories_id")
+  .join('categories','itemClass.category_id',"=","categories.category_id")
   .select('*')
 }
 
