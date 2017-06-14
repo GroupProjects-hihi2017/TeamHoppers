01d362d7e644652c1e401f5bd745db9f04c9d46a
OEM <nperret@gmail.com>
2017-06-15 10:12:29 +1200

    hash router added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
 
-const App = () => (
-  <div className='app-container'>
-    <p>Team Hoppers for the win!</p>
-  </div>
-)
+import About from './About'
+import Home from './Home'
+
+const App = () => {
+  return (
+    <Router>
+      <div className='app-container'>
+        <Route exact path='/about' component={About} />
+        <Route exact path='/' component={Home} />
+      </div>
+    </Router>
+  )
+}
 
 export default App
---
+++ client/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='homepage'>
+      <p>Go Green!</p>
+    </div>
+  )
+}
+
+export default Home
