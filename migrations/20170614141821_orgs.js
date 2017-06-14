f052f3b838cfbed425bb334b24d8cd9d794bc665
Your Name <you@example.com>
2017-06-14 16:03:12 +1200

    update seeds and migration

--- migrations/20170614141821_orgs.js
+++ migrations/20170614141821_orgs.js
@@ -1,12 +1,12 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('orgs', function (table) {
     table.increments('org_id').primary()
-    table.string('name')
-    table.string('address')
-    table.string('url')
-    table.string('img')
-    table.text('info')
-    table.string('location')
+    table.string('org_name')
+    table.string('org_address')
+    table.string('org_url')
+    table.string('org_img')
+    table.text('org_info')
+    table.string('org_location')
   })
 }
 
