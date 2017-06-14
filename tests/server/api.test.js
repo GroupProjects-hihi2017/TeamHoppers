43db7a37056bae2f3229b95c09389f8b71758107
OEM <nperret@gmail.com>
2017-06-15 11:21:47 +1200

    itemClass db tests passing

--- tests/server/api.test.js
+++ tests/server/api.test.js
@@ -2,14 +2,29 @@ var test = require('ava')
 var request = require('supertest')
 var setUpDb = require('./helpers/database-config')
 
-var db = require('../../server/db/categories')
+var categoriesDb = require('../../server/db/categories')
+var itemClassDb = require('../../server/db/itemClass')
 
 
 setUpDb(test)
 
+// testing categories
 
 test('getCategories gets all categories', (t) => {
-  return db.getCategories(t.context.db)
+  return categoriesDb.getCategories(t.context.db)
+    .then(function (result) {
+      return new Promise((resolve, reject) => {
+        var actual = result.length
+        t.is(actual, 4)
+        resolve()
+      })
+    })
+})
+
+// testing itemClass
+
+test('getItemClass gets all itemClasses', (t) => {
+  return itemClassDb.getItemClass(t.context.db)
     .then(function (result) {
       return new Promise((resolve, reject) => {
         var actual = result.length
