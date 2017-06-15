53a01035627a0e09b7c48f012b7c03a4b9db94c5
Sarah Dueweke <sdueweke@gmail.com>
2017-06-16 11:07:39 +1200

    searchbar semi-working

--- client/containers/SearchBar.jsx
+++ client/containers/SearchBar.jsx
@@ -7,31 +7,59 @@ import {Link} from 'react-router-dom'
 
 
 const renderItemInfo = (item, key) => (
-  <div className="category-box four columns" key={key}>
-
+  <div className="search-result" key={key}>
+    <ul>
+      <li>{item.itemClass_name}</li>
+    </ul>
   </div>
 )
 
 
 class SearchBar extends React.Component {
-  componentDidMount() {
-    this.props.dispatch(getOrgsByItem())
+  constructor(props){
+    super(props)
+    this.state = {
+      searchItem: '',
+      searchResults: [],
+      showMoreItems: false
+    }
   }
 
+  componentDidMount() {
+    this.props.dispatch(getOrgsByItem())
+    this.searchHandler = this.searchHandler.bind(this)
+  }
+
+  searchHandler(e){
+    this.setState({searchItem: e.target.value, searchResults: this.filterSearchItems(e.target.value, this.state.showMoreItems)})
+  }
+  showMoreItemsToggle(boolean) {
+    this.setState({showMoreItems: !boolean})
+    this.setState({searchResults: this.filterSearchItems(this.state.searchItem, !boolean)})
+  }
+
+  filterSearchItems(searchTerm, showMore){
+    if (searchTerm == '' || !searchTerm) return []
+    let searchResults = this.props.joinItemToOrgs.filter((item) => {
+      return item.itemClass_name.toLowerCase().includes(searchTerm.toLowerCase())
+    })
+    if (showMore) return searchResults
+    else return searchResults.slice(0, 4)
+  }
 
 render() {
   const {joinItemToOrgs, dispatch} = this.props
-    console.log(this.props.joinItemToOrgs);
   return (
     <div className='container'>
       <div>
         <h4 className="search-bar-header">What would you like to recycle?</h4>
       </div>
       <form>
-        <input placeholder='Search' type='text'></input>
+        <input placeholder='Search' type='text' onChange={(e) => this.searchHandler(e)}></input>
       </form>
       <div className='categories-container' >
-        {joinItemToOrgs.map((item, key) => renderItemInfo(item, key))}
+        <button onClick={() => this.showMoreItemsToggle(this.state.showMoreItems)}>{this.state.showMoreItems ? "Show Less" : "Show More"}</button>
+        {this.state.searchResults.map((item, key) => renderItemInfo(item, key))}
       </div>
     </div>
   )}
