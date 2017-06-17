410b20c0abe7d9c6e0a662604817649eb8c52c86
OEM <nperret@gmail.com>
2017-06-17 17:42:45 +1200

    removing superfluous files

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,12 +4,10 @@ import items from './items'
 import categories from './categories'
 import joinItemToOrgs from './joinItemToOrgs'
 import listOrgs from './listOrgs'
-import listItemsByOrg from './listItemsByOrg'
 
 export default combineReducers({
   items,
   categories,
   joinItemToOrgs,
-  listOrgs,
-  listItemsByOrg
+  listOrgs
 })
