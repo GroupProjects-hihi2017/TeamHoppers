1428b9a91f62bd874dfbeab706529aad03b6d089
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-06-18 15:31:01 +1200

    nav bar nearly done

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,21 +3,21 @@ import {Link} from 'react-router-dom'
 
 const Nav = () => {
   return (
-  <div className="container">
-    <div className="nav">
-      <ul>
-        <li><Link to={'/'}>Home</Link></li>
-        <li><Link to={'/organisations'}>Wellington Organisations</Link></li>
-        <li><Link to={'/general-info'}>General Info</Link></li>
-        <li><Link to={'/about'}>About</Link></li>
-        <li><Link to={'/contact'}>Contact</Link></li>
-      </ul>
-    </div>
-</div>
+    <div className = 'container'>
+    <nav className="mobile-menu">
+      <label for="show-menu" className="show-menu"><span>Menu</span><div className="lines"></div></label>
+      	<input type="checkbox" id="show-menu" />
+      		<ul id="menu">
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/organisations'}>Wellington Organisations</Link></li>
+            <li><Link to={'/general-info'}>General Info</Link></li>
+            <li><Link to={'/about'}>About</Link></li>
+            <li><Link to={'/contact'}>Contact</Link></li>
+      	</ul>
+    </nav>
+  </div>
   )
 }
-
-
 
 
 export default Nav
