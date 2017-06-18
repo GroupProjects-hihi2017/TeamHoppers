3235a7494bed36431d351c85db60d999fb28453b
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-06-18 16:09:30 +1200

    navbar nearly working. Sort of. Maybe.

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,19 +3,18 @@ import {Link} from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div className = 'container'>
-    <nav className="mobile-menu">
-      <label for="show-menu" className="show-menu"><span>Menu</span><div className="lines"></div></label>
-      	<input type="checkbox" id="show-menu" />
-      		<ul id="menu">
+      <header className = 'container'>
+        <nav>
+          <Link to="#" className="menu-icon">&#9776;</Link>
+          <ul>
             <li><Link to={'/'}>Home</Link></li>
             <li><Link to={'/organisations'}>Wellington Organisations</Link></li>
             <li><Link to={'/general-info'}>General Info</Link></li>
             <li><Link to={'/about'}>About</Link></li>
             <li><Link to={'/contact'}>Contact</Link></li>
-      	</ul>
-    </nav>
-  </div>
+          </ul>
+        </nav>
+      </header>
   )
 }
 
