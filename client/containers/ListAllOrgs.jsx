823579ed44070b89b25114d2003c9360179119a3
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-06-19 10:53:37 +1200

    toggle items working'

--- client/containers/ListAllOrgs.jsx
+++ client/containers/ListAllOrgs.jsx
@@ -1,50 +1,44 @@
 import React from 'react'
-import jump from 'jump.js'
+import ToggleDisplay from 'react-toggle-display'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 
 import {getAllOrgs} from '../actions/listOrgs'
-
-const renderAllOrgs = (org, key) => {
-  return (
-    <div className='org-box' key={key}>
-      <Link to={`/organisations/${org.org_id}`}>
-        <img className='org-logo' src={org.org_img} onClick={(e) => this.scrollToEntries(e)}/>
-        <h4 className="org-name">{org.org_name}</h4>
-      </Link>
-    </div>
-  )
-}
+import {getOrgsByItem} from '../actions/joinItemToOrgs'
+import ItemsByOrg from './ItemsByOrg'
+import OrgSingle from '../components/OrgSingle'
 
 class ListAllOrgs extends React.Component {
   componentDidMount () {
     this.props.dispatch(getAllOrgs())
-  }
-
-  scrollToEntries (e) {
-    jump('.org-item-list', {
-      offset: -50
-    })
+    this.props.dispatch(getOrgsByItem())
   }
 
   render () {
-    const {listOrgs} = this.props
+    const {listOrgs, joinItemToOrgs} = this.props
     return (
-      <div className='container'>
-        <div className='org-header'>
-          <h4>Consider recycling or donating your used items at any of these Wellington organisations:</h4>
-          <p>Want your organisation listed on our site? Get in touch with us <Link to='/contact'>here</Link>.</p>
-        </div>
-        <div className='org-link-list'>
-          {listOrgs.map((org, key) => renderAllOrgs(org, key))}
+      <div className=' container'>
+        <div className='org-container'>
+          <div className='org-header'>
+            <h4>Consider recycling or donating your used items at any of these Wellington organisations:</h4>
+            <p>Want your organisation listed on our site? Get in touch with us <Link to='/contact'>here</Link>.</p>
+          </div>
+          <div className='org-link-list'>
+            {listOrgs.map((org, key) => <OrgSingle org={org} key={key}/>)}
+          </div>
         </div>
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {listOrgs: state.listOrgs}
+
+
+function mapStateToProps(state) {
+  return {
+    listOrgs: state.listOrgs,
+    joinItemToOrgs: state.joinItemToOrgs
+  }
 }
 
 export default connect(mapStateToProps)(ListAllOrgs)
