ab93e319d9058c6a58dcad316854629aa86ea364
OEM <nperret@gmail.com>
2017-06-18 12:22:05 +1200

    db sorting by alphabetical order

--- server/db/joinItemToOrgs.js
+++ server/db/joinItemToOrgs.js
@@ -2,7 +2,7 @@ const joinItemToOrgs = (db) => {
   return db('joinItemToOrgs')
     .join('itemClass', 'itemClass.itemClass_id', '=', 'joinItemToOrgs.itemClass_id')
     .join('orgs', 'orgs.org_id', '=', 'joinItemToOrgs.org_id')
-    .select('*')
+    .select('*').orderBy('itemClass_name')
 }
 
 module.exports = {
