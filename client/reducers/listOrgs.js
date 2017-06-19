2cb0c77a0a3842d74649cbf17a39468067bfa8ad
Your Name <you@example.com>
2017-06-20 11:24:05 +1200

    maps working and header no has hyperlinks

--- client/reducers/listOrgs.js
+++ client/reducers/listOrgs.js
@@ -2,6 +2,8 @@ function listOrgs (state = [], action = {}) {
   switch (action.type) {
     case 'LIST_ORGS':
       return action.orgs
+    case 'INIT':
+      return state
     default:
       return state
   }
