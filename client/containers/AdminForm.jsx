37ef2a3c3d0c5aebc705da4899ae6043efadcf20
OEM <nperret@gmail.com>
2017-06-21 11:29:12 +1200

    working on admin forms

--- client/containers/AdminForm.jsx
+++ client/containers/AdminForm.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {browserHistory} from 'react-router'
 import {Link} from 'react-router-dom'
 
 import {addOrg} from '../actions/listOrgs'
@@ -48,24 +47,28 @@ class AdminForm extends React.Component {
   renderForm () {
     return (
       <form className='admin-form' onSubmit={(e) => this.handleSubmit(e)}>
-        <h4>Please enter the information of the organisation you would like to add to the database:</h4>
+        <h4>Add an organisation</h4>
+        <hr className='orange-hr' />
+        <p>Please enter the information of the organisation you would like to add to the database:</p>
         <input type='text' id='org_name' name='org_name' placeholder='Organisation Name' onChange={(e) => this.handleChange(e)} />
         <input type='text' id='org_address' name='org_address' placeholder='Organisation Address' onChange={(e) => this.handleChange(e)} />
         <input type='text' id='org_url' name='org_url' placeholder='Organisation Homepage URL' onChange={(e) => this.handleChange(e)} />
         <input type='text' id='org_img' name='org_img' placeholder="Link to Organisation's Logo" onChange={(e) => this.handleChange(e)} />
         <input type='text' name='org_info' placeholder="Description of Organisation" onChange={(e) => this.handleChange(e)} />
-        <div>
-          <p>{this.state.org.org_isDonatable ? "This organisation takes donations." : "This organisation recycles."}</p>
-          <input type="button" onClick={(e) => this.toggleDonatable(e)} value="Click to Choose Donate or Recycle" />
+        <div className="add-org">
+          <p className="donate-or-recycle">{this.state.org.org_isDonatable ? "This organisation takes donations." : "This organisation recycles."}</p>
+          <input className="button" type="button" onClick={(e) => this.toggleDonatable(e)} value="Click to Choose Donate or Recycle" />
+          <input className="submit" type='submit' id='submit' value='Submit New Organisation' />
         </div>
-        <input className="submit" type='submit' id='submit' value='Submit New Organisation' />
       </form>
     )
   }
   render () {
     return (
-      <div className='container'>
-        {this.state.submitted ? this.renderMessage() : this.renderForm() }
+      <div className = 'wallpaper-no-border'>
+        <div className='container form'>
+          {this.state.submitted ? this.renderMessage() : this.renderForm() }
+        </div>
       </div>
     )
   }
