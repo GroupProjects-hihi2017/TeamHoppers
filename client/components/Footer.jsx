9408c2b4098da7e89704b7774dfc34efc2e74d13
OEM <nperret@gmail.com>
2017-06-20 14:28:13 +1200

    footer slogan added

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => {
         <div className='footer-text'>
           <span className='recycle'>recycle</span>
           <span className='hack'>hack</span>
+          <p className='footer-slogan'>Thank you for recycling!</p>
         </div>
         <img className='footer-img' src='http://2ej3yy3dhmmm499wwc3l4n36.wpengine.netdna-cdn.com/wp-content/themes/leeds-sustainability/img/bike-cycle-spot-wobble-2.gif'/>
       </div>
