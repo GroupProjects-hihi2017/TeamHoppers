e46fdb1f338e75703d1f3e4a5396467b008d5536
Sarah Dueweke <sdueweke@gmail.com>
2017-07-10 11:12:36 +1200

    add bootstrap to responsive navbar

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -2,42 +2,30 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 
+
 class Nav extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      toggleNav: false
-    }
-  }
-  handleClick (toggleNav, e) {
-    this.setState({toggleNav})
-    e.preventDefault();
-  }
 
   render () {
-    let {toggleNav} = this.state
     return (
-      <div className='no-border nav-box'>
-        <header className = 'container'>
-          <nav>
-            <a href="#" onClick={(e) => this.handleClick(!toggleNav, e)} className='menu-icon'>&#9776;</a>
-            <ul className={this.state.toggleNav ? 'toggle-on' : 'toggle-off'}>
-              <li><Link to={'/'}>Home</Link></li>
-              <li><Link to={'/organisations'}>Wellington Organisations</Link></li>
-              <li><Link to={'/general-info'}>General Info</Link></li>
-              <li><Link to={'/about'}>About</Link></li>
-              <li><Link to={'/contact'}>Contact</Link></li>
+      <nav className="navbar navbar-inverse">
+        <div className='container-fluid'>
+          <div className='navbar-header'>
+            <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='#mainNavBar'> &#9776;﻿</button>
+          </div>
+          <div className='collapse navbar-collapse' id='mainNavBar'>
+            <ul className='nav navbar-nav'>
+                <li><Link to={'/'} className='nav-item'>Home</Link></li>
+                <li><Link to={'/organisations'} className='nav-item'>Wellington Organisations</Link></li>
+                <li><Link to={'/general-info'} className='nav-item'>General Info</Link></li>
+                <li><Link to={'/about'} className='nav-item'>About</Link></li>
+                <li><Link to={'/contact'} className='nav-item'>Contact</Link></li>
             </ul>
-          </nav>
-        </header>
-      </div>
+          </div>
+        </div>
+      </nav>
     )
   }
 }
-const mapStateToProps = (state) => {
-  return {
-    toggleNav: state.toggleNav
-  }
-}
 
-export default connect(mapStateToProps)(Nav)
+
+export default Nav
